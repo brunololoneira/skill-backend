ac6183e9ea148bae36ce3d584f43aaee20874a7f
uo289270 <uo289270@uniovi.es>
2025-10-07 21:35:31 +0200

    rutas de tutorias

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { requireAuth } = require('../middlewares/auth.middleware');
+const { requireAuth } = require('../middlewares/validators/auth.middleware');
 const { associateTutelado, myTutelados } = require('../controllers/user.controller');
 
 router.post('/associate', requireAuth, associateTutelado);
