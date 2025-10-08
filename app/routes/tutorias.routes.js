c73901ee7f16732b9901607a79fbec972f4b1c45
uo289270 <uo289270@uniovi.es>
2025-10-08 20:26:19 +0200

    modificacion rutas /tutorias

--- app/routes/tutorias.routes.js
+++ app/routes/tutorias.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { requireAuth } = require('../middlewares/validators/auth.middleware');
 const { asociarTutoriaValidator } = require('../middlewares/validators/tutorias.validator');
 const { associateTutelado, myTutelados } = require('../controllers/tutorias.controller');
+console.log ({ requireAuth, asociarTutoriaValidator, associateTutelado, myTutelados });
 
 router.post('/asociar', requireAuth, asociarTutoriaValidator, associateTutelado);
 router.get('/tutelados',  requireAuth, myTutelados);
